6e61cc55dfd9b4de2d5900026cd61f036b8f55c9
lucianocretella95 <luciano.cretella@tim.it>
2024-05-29 16:29:12 +0200

    importo axios

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <script>
+import axios from 'axios';
+import main from './components/main.vue';
+export default {
+  components: {
+    main,
+  }
+}
 </script>
 
 <template>
+  <main />
 </template>
 
 <style scoped></style>
