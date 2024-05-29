804be88ff2a27abcca3f91d8c47f8a1d985c9a83
lucianocretella95 <luciano.cretella@tim.it>
2024-05-29 17:22:03 +0200

    tentativo di stampare i dati da API

--- src/components/main.vue
+++ src/components/main.vue
@@ -3,23 +3,22 @@ import axios from "axios";
 export default {
     options: {
         params: { include_adult: 'false', language: 'en-US', page: '1' },
+    },
+    methods: {
+        options() {
+            axios.get('https://api.themoviedb.org/3/search/movie?api_key=d7e474ca7946c8bafc272a2058d56cb0&query= INPUT UTENTE')
+                .then(function (result) {
+                    console.log(result.data);
+                });
+        }
     }
-}
-methods: {
-    options() {
-        axios.get('https://api.themoviedb.org/3/search/movie?api_key=d7e474ca7946c8bafc272a2058d56cb0&query= INPUT UTENTE')
-            .then(function (result) {
-                console.log(result.data);
-            });
-    }
-}
-
+};
 </script>
 
 <template>
     <div class="navbar">
         <h1>Boolflix</h1>
-        <button @click="request">cerca film</button>
+        <button @click="options">cerca film</button>
     </div>
 </template>
 
