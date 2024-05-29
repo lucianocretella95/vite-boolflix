898f1479a6f6baee5515cde67ace94859eb66522
lucianocretella95 <luciano.cretella@tim.it>
2024-05-29 16:55:53 +0200

    richiamo API

--- src/components/main.vue
+++ src/components/main.vue
@@ -2,22 +2,18 @@
 import axios from "axios";
 export default {
     options: {
-        method: 'GET',
-        url: 'https://api.themoviedb.org/3?api_key=d7e474ca7946c8bafc272a2058d56cb0/movies',
         params: { include_adult: 'false', language: 'en-US', page: '1' },
-        headers: {
-            accept: 'application/json',
-            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkN2U0NzRjYTc5NDZjOGJhZmMyNzJhMjA1OGQ1NmNiMCIsInN1YiI6IjY2NTczM2UxZTU3MjdjNDE2OTFhMWFhYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.btxNRj6OpZw8zU1TeDw-tgmXpNAdknVbaoCBcfMX4f4'
-        }
     }
 }
-    .request(options)
-    .then(function (response) {
-        console.log(response.data);
-    })
-    .catch(function (error) {
-        console.error(error);
-    });
+methods: {
+    options() {
+        axios.get('https://api.themoviedb.org/3/search/movie?api_key=d7e474ca7946c8bafc272a2058d56cb0&query= INPUT UTENTE')
+            .then(function (result) {
+                console.log(result.data);
+            });
+    }
+}
+
 </script>
 
 <template>
